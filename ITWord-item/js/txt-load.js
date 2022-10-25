079c7ba05d6a84108884d257c030bd74330b65c7
yoloy001 <102847911+yoloy001@users.noreply.github.com>
2022-10-25 17:56:05 +0800

    Add files via upload

---
+++ ITWord-item/js/txt-load.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+
+	var aBox = document.getElementById('listLeft');
+	var aList = aBox.getElementsByTagName('a');
+	for (var i = 0; i < aList.length; i++) {
+		aList[i].onclick = function(i) {
+			return function() {
+				txtUrl = [
+					"pc-black","pc-blue","pc-low","print",
+					"hw-swith","cisco-swith",
+					"bat","","",
+					"os-soft"
+				]
+				if (i!==0 && i!==7 && i!==8) {
+					$("#pc-black").load("txt/"+txtUrl[i]+".txt");
+				} else{
+					location.reload();
+				}
+				
+			}
+		}(i);
+	}
+}
+
+function copyfun() {
+	var e = document.getElementById("copyvalue");
+	e.select(); // 选择对象
+	document.execCommand("Copy"); // 执行浏览器复制命令
+	// alert("复制成功");
+}
